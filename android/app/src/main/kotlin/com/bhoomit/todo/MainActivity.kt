c818cabbcaacc0b64e7ecd8f7fae1f122705def2
bhoomit <bhfirebase74@gmail.com>
2024-06-28 22:53:18 +0530

    Setup project

---
+++ android/app/src/main/kotlin/com/bhoomit/todo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bhoomit.todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo/presentation/pages/dashboard/dashboard.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODO App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Dashboard(),
+    );
+  }
+}
