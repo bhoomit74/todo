0e6d98b97db5447adf8e6c7bc98e9c439ef3a74c
bhoomit <bhfirebase74@gmail.com>
2024-06-29 15:16:26 +0530

    feat: Add AppChip widget.

---
+++ lib/presentation/widgets/app_chip.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:todo/presentation/styles/styles.dart';
+
+class AppChip extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final Function() onPressed;
+  const AppChip(
+      {super.key,
+      required this.label,
+      this.isSelected = false,
+      required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Chip(
+        label: Text(label,
+            style: isSelected ? AppTextStyle.activeChip : AppTextStyle.chip),
+        backgroundColor: isSelected ? AppColors.blue100 : Colors.transparent,
+        side: const BorderSide(color: Colors.transparent, width: 0),
+        padding: AppConstants.padding14,
+        shape:
+            RoundedRectangleBorder(borderRadius: AppConstants.borderRadius32),
+      ),
+    );
+  }
+}
